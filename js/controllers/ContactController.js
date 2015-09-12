4b9bec45f4bbd578f44fb94d399b4ca847fbc413
CuriousLearner <sanyam.khurana01@gmail.com>
2015-09-12 13:35:01 +0530

    Contact basic front-end done.

--- js/controllers/ContactController.js
+++ js/controllers/ContactController.js
@@ -1,3 +1,20 @@
-app.controller('ContactController', function($scope) {
-  // alert("ContactController");
+app.controller('ContactController', function($scope, $http) {
+  $scope.user = {};
+  $(document).ready(function() {
+    $('textarea#message').characterCounter();
+  });
+  $scope.submit = function() {
+    console.log($scope.user);
+    $http({
+      method: 'POST', 
+      url: 'http://localhost:5000/api/send-contact-us-form/', 
+      data: $scope.user, 
+      }).success(function(data){
+        $scope.result = data.Message;
+      }).
+      error(function(data){
+        console.log("Unable to submit form");
+        $scope.result = data.Message;
+      });
+    };
 });
